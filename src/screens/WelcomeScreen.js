8db5627028fb7bca5a098d30520d8bfafaf78307
kevin <bkevvc@gmail.com>
2024-09-10 12:13:00 +0800

    Initial UI setup for welcome page

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Image, StyleSheet, TouchableOpacity, Text } from 'react-native';
+
+const WelcomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      {/* Logo Image */}
+      <Image 
+        source={require('../assets/tranqheal-logo.png')}  // Replace with the actual path to your logo file
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      
+      {/* Get Started Button */}
+      <TouchableOpacity 
+        style={styles.button} 
+        onPress={() => navigation.navigate('RegisterAs')}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CBC3E3', // Background color or gradient
+  },
+  logo: {
+    width: 250,  // Adjust width and height based on logo dimensions
+    height: 250,
+    marginBottom: 100,
+  },
+  button: {
+    backgroundColor: '#fff',
+    paddingVertical: 15,
+    paddingHorizontal: 60,
+    borderRadius: 30,
+    elevation: 3,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#000',
+  },
+});
+
+export default WelcomeScreen;
