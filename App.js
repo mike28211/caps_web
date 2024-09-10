9716aa7639c2028c7feaabda17de2109dcd32f4f
mike28211 <mikkominoza@gmail.com>
2024-09-10 14:42:55 +0800

    homepage

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
+import {createNativeStackNavigator} from '@react-navigation/stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>TRANQHEAL</Text>
-      <StatusBar style="auto" />
-    </View>
+ 
+    <MainContainer/>
+ 
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D7CCF2',
     alignItems: 'center',
     justifyContent: 'center',
   },
