867025a1de9aebd7b12973dd170d931887bb3351
kevin <bkevvc@gmail.com>
2024-09-19 16:52:19 +0800

    Restructure the folder structure for src

--- src/screens/SelfAssessmentScreen.js
+++ src/screens/SelfAssessmentScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function SelfAssessmentScreen() {
+export const SelfAssessmentScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>SELF ASSESSMENT</Text>
---
+++ src/screens/index.js
@@ -0,0 +1,15 @@
+import { WelcomeScreen } from './WelcomeScreen';
+import { RegisterAsScreen } from './RegisterAsScreen';
+import { LoginScreen } from './LoginScreen';
+import { SignupScreen } from './SignupScreen';
+import { HomeScreen } from './HomeScreen';
+import { SelfAssessmentScreen } from './SelfAssessmentScreen';
+import { ForumsScreen } from './ForumsScreen';
+import { ViewProfScreen } from './ViewProfScreen';
+import { ViewOrgScreen } from './ViewOrgScreen';
+import { MoodScreen } from './MoodScreen';
+import { ProfileScreen } from './ProfileScreen';
+import { NotificationScreen } from './NotificationScreen';
+import { MenuScreen } from './MenuScreen';
+
+export { WelcomeScreen, RegisterAsScreen, LoginScreen, SignupScreen, ViewProfScreen, ViewOrgScreen, ForumsScreen, HomeScreen, SelfAssessmentScreen, MoodScreen, ProfileScreen, NotificationScreen, MenuScreen };
